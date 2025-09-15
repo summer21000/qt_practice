13bde96b48d850941c0fe563166cca17b2513bff
summer21000 <wkrehreo1999@gmail.com>
2025-09-15 17:32:20 +0900

    2_출력 방식 변경
    일반 cout에서 qDebug방식으로 문자열 출력 방식 변경

--- parctice1_hello/hello/main.cpp
+++ parctice1_hello/hello/main.cpp
@@ -1,11 +1,13 @@
 #include <QCoreApplication>
-#include <iostream>
+//#include <iostream> 일반 c++ 출력 방식
+#include <QDebug> //Qt의 부가적인 출력방식
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    std::cout<<"Hello Qt";
+    //std::cout<<"Hello Qt!"; 일반 C++출력방식
+    qDebug() <<"Hello Qt!"; //Qt의 부가적인 출력방식
 
     return a.exec();
 }
