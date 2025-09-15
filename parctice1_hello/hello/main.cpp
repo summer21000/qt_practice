793967b61fafb57ba0184c466e3b383f9cbad3db
summer21000 <wkrehreo1999@gmail.com>
2025-09-15 17:26:49 +0900

    1_first commit
    qt 연습용 리포지토리 커밋

---
+++ parctice1_hello/hello/main.cpp
@@ -0,0 +1,11 @@
+#include <QCoreApplication>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    std::cout<<"Hello Qt";
+
+    return a.exec();
+}
