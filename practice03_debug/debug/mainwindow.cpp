a1239fa7d0f6aefde862ec1bae6c3baaf214eb39
summer21000 <wkrehreo1999@gmail.com>
2025-09-16 09:55:24 +0900

    8_세번째 예제 디버그
    디버그 예제 커밋

--- practice03_debug/debug/mainwindow.cpp
+++ practice03_debug/debug/mainwindow.cpp
@@ -6,6 +6,35 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // qDebug 테스트
+    qDebug() << "hello";
+    qDebug() << "hello" << "Qt";
+    qDebug("hello");
+    qDebug() << QString("hello");
+
+    // string 변수 사용
+    QString str1, str2;
+    str1 = "hello";
+    str2 = "Qt";
+    qDebug() << QString("%1 Qt").arg(str1);
+    qDebug() << QString("%1 %2").arg(str1).arg(str2);
+    qDebug() << "hi Qt! " << QString("%1").arg(str1);
+
+    // 정수형 변수 사용
+    int n = 10;
+    qDebug() << n;
+
+    // 변수의 상수화(const 사용)
+    // C++ 계열에서는 #define 보다는 const를 사용
+    double const PI = 3.141492;
+    qDebug() << PI;
+
+    //사칙연산 가능
+    qDebug() << (n*PI) + n;
+
+    //논리연산 가능
+    qDebug() << ((n < 11) && (n > 0));
 }
 
 MainWindow::~MainWindow()
