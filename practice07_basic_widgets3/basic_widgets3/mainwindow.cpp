078ddf0958e0882dd550c35387f51daec3a653da
summer21000 <wkrehreo1999@gmail.com>
2025-09-17 14:03:58 +0900

    22_일곱번째 예제
    중간커밋

--- practice07_basic_widgets3/basic_widgets3/mainwindow.cpp
+++ practice07_basic_widgets3/basic_widgets3/mainwindow.cpp
@@ -6,9 +6,38 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    // 최초실행시, 프로그램이 실행되고 있는 파일경로 가져오기
+    ui->lineEdit_1_address->setText(QCoreApplication::applicationDirPath());
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+// 경로지정 버튼 클릭시 호출 슬롯
+void MainWindow::on_pushButton_1_pointAddress_clicked()
+{
+    // 파일 탐색기창 제목
+
+}
+
+void MainWindow::on_pushButton_2_openAddress_clicked()
+{
+
+}
+
+void MainWindow::on_pushButton_3saveSetting_clicked()
+{
+
+}
+
+void MainWindow::on_pushButton_4_openfile_clicked()
+{
+
+}
+
+void MainWindow::on_pushButton_5_apply_clicked()
+{
+
 }
--- practice07_basic_widgets3/basic_widgets3/mainwindow.h
+++ practice07_basic_widgets3/basic_widgets3/mainwindow.h
@@ -2,6 +2,21 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QDebug>                                       // QDebug add
+
+// 파일탐색기 창을 사용하기 위해 삽입
+#include <QFileDialog>
+#include <QDesktopServices>
+
+// 파일 쓰기/읽기용 삽입
+#include <QFile>
+#include <QTextStream>
+
+// 메시지 알림창을 사용하기 위해 삽입
+#include <QMessageBox>
+
+// 현재시각을 받아오기 위해 삽입
+#include <QDateTime>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +30,19 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_1_pointAddress_clicked();
+    void on_pushButton_2_openAddress_clicked();
+    void on_pushButton_3saveSetting_clicked();
+    void on_pushButton_4_openfile_clicked();
+    void on_pushButton_5_apply_clicked();
+
+    // 스트링 타입변수로 반환하는 슬롯 선언
+    QString return_setup();
+
 private:
     Ui::MainWindow *ui;
+    QString global_file_contents;
 };
 
 #endif // MAINWINDOW_H
